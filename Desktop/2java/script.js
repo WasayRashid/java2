f4f4653eaf743255ca94a10c11dc5a989e4f0b34
WasayRashid <gmeraniq@gmail.com>
2024-07-06 08:28:12 -0700

    first commit

---
+++ Desktop/2java/script.js
@@ -0,0 +1,34 @@
+// var email = prompt("Enter your email")
+// var  pasword = prompt("Enter your passwrd")
+
+
+// if (email==="kashan@gmail.com"){
+//     alert ("you are kashan")
+
+// } else if(pasword="kashan")
+//     alert("you are admin")
+// else(
+//     alert("you are user")
+// )
+// var gmail = prompt("enter your email")
+
+
+// if
+// (gmail === "admin@gmail.com" ){
+
+//     var password = prompt("enter your password")
+
+// if(password === "admin"){
+ 
+//     alert("you are admin")
+// }
+// }
+var schools=["smart","allied","native"]
+var school=prompt("enter your school name")
+if(schools[0]===school){alert(""+schools[0])}
+if(schools[1]===school){alert(""+schools[1])}
+if(schools[2]===school){alert(""+schools[2])}
+else{
+
+    alert("this school is not in list")
+}
